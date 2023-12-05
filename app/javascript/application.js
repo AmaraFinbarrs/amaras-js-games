a52d734bfb4431fb637ce1e10d51569969a99438
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-12-05 18:53:03 +0000

    Configure js files properly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
+//=require_directory .
+//=require_self
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import "./index"
